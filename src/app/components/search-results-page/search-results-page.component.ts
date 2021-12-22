16dd3f5c8b7ccf03444616d90427ed9dc4d4cbd0
sika2001 <sika2001@student.miun.se>
2021-12-22 14:41:54 +0100

    added search results page

---
+++ src/app/components/search-results-page/search-results-page.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BookService } from '../../services/book.service';
+import { Book } from '../../interfaces/book';
+
+@Component({
+  selector: 'app-search-results-page',
+  templateUrl: './search-results-page.component.html',
+  styleUrls: ['./search-results-page.component.css'],
+})
+export class SearchResultsPageComponent implements OnInit {
+  books: Book[] = [];
+  imageHeight: number = 192;
+  maxResults: number = 40;
+
+  constructor(private bookService: BookService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      if (params['find'] === 'book') {
+        this.bookService.searchBooksByTitle(params['keyword'], this.maxResults)
+          .subscribe((books) => { this.books = books; });
+      } else if (params['find'] === 'author') {
+        this.bookService.searchBooksByAuthor(params['keyword'], this.maxResults)
+          .subscribe((books) => { this.books = books; });
+      }
+    });
+  }
+}

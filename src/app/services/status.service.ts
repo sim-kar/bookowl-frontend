81e77c9f2bd54b3c9cc53762cc85ad43bf00db8c
sika2001 <sika2001@student.miun.se>
2021-12-25 12:06:07 +0100

    updated status service to allow posting book with status

--- src/app/services/status.service.ts
+++ src/app/services/status.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { Book } from '../interfaces/book';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +12,12 @@ export class StatusService {
 
   constructor(private http: HttpClient) { }
 
-  addStatus(isbn: string, username: string, status: number): Observable<{}> {
-    return this.http.post(`${this.apiBaseURL}/statuses`, { isbn, username, status });
+  addStatus(isbn: string, username: string, status: number, book?: Book): Observable<{}> {
+    return this.http.post(`${this.apiBaseURL}/statuses`, {
+      isbn,
+      username,
+      status,
+      book,
+    });
   }
 }

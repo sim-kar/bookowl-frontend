644651e0a90f070f1431ed49ac16d7deb878a00e
Simon Karlsson <a.simon.karlsson@gmail.com>
2023-07-24 17:54:50 +0200

    updated to only enforce https in production environment

--- src/app/http-interceptors/ensure-https-interceptor.ts
+++ src/app/http-interceptors/ensure-https-interceptor.ts
@@ -6,10 +6,15 @@ import {
   HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class EnsureHttpsInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (environment.production === false) {
+      return next.handle(req);
+    }
+
     // clone request and replace 'http://' with 'https://'
     const secureReq = req.clone({
       url: req.url.replace('http://', 'https://'),
